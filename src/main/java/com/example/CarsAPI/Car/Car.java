51f21be2365cae27a386418df43f1262fe6832ac
BrandonDeoram <brandondeoram8931@gmail.com>
2023-06-12 14:31:25 -0400

    added Car class

---
+++ src/main/java/com/example/CarsAPI/Car/Car.java
@@ -0,0 +1,11 @@
+package com.example.CarsAPI.Car;
+
+public class Car {
+    private String make;
+    private String model;
+    private String type;
+    private String color;
+    private String engine;
+    private int year;
+    private int horsepower;
+}
